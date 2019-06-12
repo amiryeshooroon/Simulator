3f99368782606aa25755c1b441944ed5e8c915d3
amir <guyamirmichlala@gmail.com>
2019-06-13 02:28:39 +0300

    Added Platform.runAfter

--- src/View/JoyStick.java
+++ src/View/JoyStick.java
@@ -1,19 +1,21 @@
 package View;
 
+import javafx.application.Platform;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.Region;
 import javafx.scene.paint.Paint;
 import javafx.scene.shape.Circle;
 
-public class JoyStick extends Pane {
+public class JoyStick extends Region {
     private Circle bigCircle;
     private Circle smallCircle;
 
     public void displayJoystick(){
-        setWidth(200);
-        setHeight(200);
-        bigCircle = new Circle(getWidth() / 2, getHeight() / 2, getWidth() / 2, Paint.valueOf("#0000FF"));
-        smallCircle = new Circle(getWidth() / 2, getHeight() / 2, getWidth() / 4, Paint.valueOf("#FF0000"));
-        getChildren().add(bigCircle);
-        getChildren().add(smallCircle);
+        Platform.runLater(()-> {
+            bigCircle = new Circle(getWidth() / 2, getHeight() / 2, getWidth() / 2, Paint.valueOf("#0000FF"));
+            smallCircle = new Circle(getWidth() / 2, getHeight() / 2, getWidth() / 4, Paint.valueOf("#FF0000"));
+            super.getChildren().add(bigCircle);
+            super.getChildren().add(smallCircle);
+        });
     }
 }
