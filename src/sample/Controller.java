f3475a138c8380d834c66771381d759e3a8bade7
amir <guyamirmichlala@gmail.com>
2019-06-13 02:13:16 +0300

    Added old controller

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,22 @@
 package sample;
 
-public class Controller {
+import View.JoyStick;
+import View.MapDisplayer;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.Arrays;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+    @FXML
+    MapDisplayer mapDisplayer;
+    @FXML
+    JoyStick joyStick;
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        mapDisplayer.displayMap(Arrays.asList(Arrays.asList(1.0,1.0,1.0), Arrays.asList(1.0,1.0,1.0), Arrays.asList(1.0,1.0,1.0)));
+        joyStick.displayJoystick();
+    }
 }
