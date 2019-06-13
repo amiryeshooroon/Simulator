862df845fa5bef42e3ce6077ff56f51639473f71
amir <guyamirmichlala@gmail.com>
2019-06-13 03:31:05 +0300

    * Added distance calculator * Added Point * Circles are now working as a joystick

---
+++ src/Utilities/CircleLine.java
@@ -0,0 +1,40 @@
+package Utilities;
+
+import java.util.Collections;
+
+public class CircleLine {
+    public static Point getCircleLineIntersectionPoint(Point pointA,
+                                                             Point pointB, Point center, double radius) {
+        double baX = pointB.x - pointA.x;
+        double baY = pointB.y - pointA.y;
+        double caX = center.x - pointA.x;
+        double caY = center.y - pointA.y;
+
+        double a = baX * baX + baY * baY;
+        double bBy2 = baX * caX + baY * caY;
+        double c = caX * caX + caY * caY - radius * radius;
+
+        double pBy2 = bBy2 / a;
+        double q = c / a;
+
+        double disc = pBy2 * pBy2 - q;
+        if (disc < 0) {
+            return null;
+        }
+        // if disc == 0 ... dealt with later
+        double tmpSqrt = Math.sqrt(disc);
+        double abScalingFactor1 = -pBy2 + tmpSqrt;
+        double abScalingFactor2 = -pBy2 - tmpSqrt;
+
+        Point p1 = new Point(pointA.x - baX * abScalingFactor1, pointA.y
+                - baY * abScalingFactor1);
+        if (disc == 0) { // abScalingFactor1 == abScalingFactor2
+            return p1;
+        }
+        Point p2 = new Point(pointA.x - baX * abScalingFactor2, pointA.y
+                - baY * abScalingFactor2);
+        double d1 = Math.hypot(Math.abs(pointB.x - p1.x), Math.abs(pointB.y - p1.y));
+        double d2 = Math.hypot(Math.abs(pointB.x - p2.x), Math.abs(pointB.y - p2.y));
+        return d1 < d2 ? p1 : p2;
+    }
+}
--- src/View/JoyStick.java
+++ src/View/JoyStick.java
@@ -1,5 +1,7 @@
 package View;
 
+import Utilities.CircleLine;
+import Utilities.Point;
 import javafx.application.Platform;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.Region;
@@ -10,12 +12,40 @@ public class JoyStick extends Region {
     private Circle bigCircle;
     private Circle smallCircle;
 
+
     public void displayJoystick(){
         Platform.runLater(()-> {
             bigCircle = new Circle(getWidth() / 2, getHeight() / 2, getWidth() / 2, Paint.valueOf("#0000FF"));
-            smallCircle = new Circle(getWidth() / 2, getHeight() / 2, getWidth() / 4, Paint.valueOf("#FF0000"));
+            smallCircle = new Circle(getWidth() / 2, getHeight() / 2, getWidth() / 8, Paint.valueOf("#FF0000"));
             super.getChildren().add(bigCircle);
             super.getChildren().add(smallCircle);
+            smallCircle.setOnMouseDragged(event->{
+                if(Math.hypot(Math.abs(event.getX() - bigCircle.getCenterX()), Math.abs(event.getY() - bigCircle.getCenterY())) > bigCircle.getRadius()){
+                    Point p = CircleLine.getCircleLineIntersectionPoint(new Point(bigCircle.getCenterX(), bigCircle.getCenterY()), new Point(event.getX(), event.getY())
+                            , new Point(bigCircle.getCenterX(), bigCircle.getCenterY()), bigCircle.getRadius());
+                    smallCircle.setCenterX(p.getX());
+                    smallCircle.setCenterY(p.getY());
+                    return;
+                }
+                smallCircle.setCenterX(event.getX());
+                smallCircle.setCenterY(event.getY());
+            });
+            smallCircle.setOnMouseReleased(event->{
+                Point p = null;
+                if(Math.hypot(Math.abs(event.getX() - bigCircle.getCenterX()), Math.abs(event.getY() - bigCircle.getCenterY())) > bigCircle.getRadius()) {
+                    p = CircleLine.getCircleLineIntersectionPoint(new Point(bigCircle.getCenterX(), bigCircle.getCenterY()), new Point(event.getX(), event.getY())
+                            , new Point(bigCircle.getCenterX(), bigCircle.getCenterY()), bigCircle.getRadius());
+                }
+                else p = new Point(event.getX(), event.getY());
+                for(int i=1;i<=9999999;i++) {
+                    double x = (bigCircle.getCenterX() * i + p.getX() * (9999999-i)) / 9999999;
+                    double y = (bigCircle.getCenterY() * i + p.getY() * (9999999-i)) / 9999999;
+                    smallCircle.setCenterX(x);
+                    smallCircle.setCenterY(y);
+                }
+                smallCircle.setCenterX(bigCircle.getCenterX());
+                smallCircle.setCenterY(bigCircle.getCenterY());
+            });
         });
     }
 }
