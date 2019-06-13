ebef92631a818d04485cedc280e55ea3980b35c9
amir <guyamirmichlala@gmail.com>
2019-06-13 19:42:46 +0300

    * Moved controller to a new one inside view package * Added file dialog

--- src/View/MainWindowController.java
+++ src/View/MainWindowController.java
@@ -1,22 +1,37 @@
-package sample;
+package View;
 
-import View.JoyStick;
-import View.MapDisplayer;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.stage.FileChooser;
+import sample.Main;
 
+import java.io.File;
 import java.net.URL;
 import java.util.Arrays;
+import java.util.Observable;
+import java.util.Observer;
 import java.util.ResourceBundle;
 
-public class Controller implements Initializable {
+public class MainWindowController implements Initializable, Observer {
     @FXML
     MapDisplayer mapDisplayer;
     @FXML
     JoyStick joyStick;
+
+    @Override
+    public void update(Observable o, Object arg) {
+
+    }
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         mapDisplayer.displayMap(Arrays.asList(Arrays.asList(1.0,1.0,1.0), Arrays.asList(1.0,1.0,1.0), Arrays.asList(1.0,1.0,1.0)));
         joyStick.displayJoystick();
+    }
+
+    public void onOpenFileClick(){
+        FileChooser fileChooser = new FileChooser();
+        File file = fileChooser.showOpenDialog(Main.stage);
+
     }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -7,9 +7,10 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
+    public static Stage stage;
     @Override
     public void start(Stage primaryStage) throws Exception{
+        stage = primaryStage;
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
